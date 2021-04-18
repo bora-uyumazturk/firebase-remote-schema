199e5221be59209007ef5c6882b2f075f3ebc39d
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-04-18 17:51:25 -0400

    change type to users with email

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -6,17 +6,16 @@ import { gql } from "apollo-server";
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  # This "User" type defines the queryable fields for every registered user.
+  type User {
+    email: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
+  # case, the "users" query returns an array of zero or more users (defined above).
   type Query {
-    books: [Book]
+    users: [User]
   }
 `;
 
