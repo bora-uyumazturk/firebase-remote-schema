6289825b7a2c44d0af5aeb9d6412beff28f56878
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-04-18 17:47:33 -0400

    move resolvers into their own folder

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import { ApolloServer } from "apollo-server";
 
 import typeDefs from "./typeDefs";
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+import resolvers from "./resolvers";
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
