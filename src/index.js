ac691fc98f722f01012b5847b7715d970308e4a2
bora-uyumazturk <bora.uyumazturk@gmail.com>
2021-04-18 22:47:34 -0400

    added auth with jwt

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import { ApolloServer } from "apollo-server";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { ApolloServer, AuthenticationError } from "apollo-server";
+
+import jwt from "jsonwebtoken";
 
 import typeDefs from "./typeDefs";
 import resolvers from "./resolvers";
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    try {
+      const token = req.get("Authorization").split(" ")[1];
+      jwt.verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+      throw new AuthenticationError("Invalid authorization!");
+    }
+  },
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,7 @@ import { listAllUsers, getUserByEmail } from "../utils/auth";
 
 const Query = {
   users: () => listAllUsers(),
-  user: (args) => getUserByEmail(args.email),
+  user: (parent, args) => getUserByEmail(args.email),
 };
 
 export default Query;
